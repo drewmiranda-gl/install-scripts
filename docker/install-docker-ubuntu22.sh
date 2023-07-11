21c83877833feda23f389a97c86a884bebcb1420
Drew Miranda <drew.miranda@graylog.com>
2023-07-11 16:13:09 -0500

    add

---
+++ docker/install-docker-ubuntu22.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+root_check() {
+    get_curusr=$(whoami)
+    if [ $get_curusr == "root" ]
+    then
+        ok="okhere"
+    else
+        echo "ERROR! Please run as root."
+        echo "Try: 'sudo su' to elevate and then run install script again."
+	echo "OR sudo bash $0"
+        exit 1
+    fi
+}
+root_check
+
+sudo apt update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+
+sudo docker run hello-world
