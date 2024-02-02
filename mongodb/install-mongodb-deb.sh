006ee4992426c91123a0f8ac5493739791904906
Drew Miranda <drew.miranda@graylog.com>
2024-02-02 12:12:46 -0500

    add mongo

---
+++ mongodb/install-mongodb-deb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+root_check() {
+    get_curusr=$(whoami)
+    if [ $get_curusr == "root" ]
+    then
+        ok="okhere"
+    else
+        echo "ERROR! Please run as root."
+        echo "Try: 'sudo su' to elevate and then run install script again."
+	echo "OR sudo bash $0"
+        exit 1
+    fi
+}
+root_check
+
+# for reference, via https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update
+sudo apt install -y mongodb-org mongodb-mongosh
