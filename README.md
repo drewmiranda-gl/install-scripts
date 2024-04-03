c9ec6af87eb34dd5d14286c8c48bbe0caf6b6694
Drew Miranda <drew.miranda@graylog.com>
2024-04-03 15:00:00 -0400

    add

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Helper scripts to install common things
 This will be primarily focused on ubuntu server.
 
 At the time of writing, the version i'm testing with is **Ubuntu Server 22.04** LTS.
+
+These scripts are provided as-is, and without any warranty or guarantee. They do not imply any statements of support and cannot be guaranteed to work in the future.
---
+++ graylog/server/lab-or-test-install/install-graylog-5_2.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+root_check() {
+    get_curusr=$(whoami)
+    if [ $get_curusr == "root" ]
+    then
+        ok="okhere"
+    else
+        echo "ERROR! Please run as root."
+        echo "Try: 'sudo su' to elevate and then run install script again."
+	echo "OR sudo bash $0"
+        exit 1
+    fi
+}
+root_check
+
+# =============================================================================
+# OpenSearch
+
+# verify prereqs are present
+sudo apt-get update && sudo apt-get -y install lsb-release ca-certificates curl gnupg2
+# download signing key
+curl -o- https://artifacts.opensearch.org/publickeys/opensearch.pgp | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/opensearch-keyring
+# create repository file
+echo "deb [signed-by=/usr/share/keyrings/opensearch-keyring] https://artifacts.opensearch.org/releases/bundle/opensearch/2.x/apt stable main" | sudo tee /etc/apt/sources.list.d/opensearch-2.x.list
+# install opensearch
+sudo apt-get update
+sudo OPENSEARCH_INITIAL_ADMIN_PASSWORD=$(tr -dc A-Z-a-z-0-9_@#%^-_=+ < /dev/urandom  | head -c${1:-32}) apt-get -y install opensearch
+
+sudo cp /etc/opensearch/opensearch.yml /etc/opensearch/opensearch.yml.bak
+
+echo "cluster.name: graylog
+node.name: ${HOSTNAME}
+path.data: /var/lib/opensearch
+path.logs: /var/log/opensearch
+transport.host: 0.0.0.0
+network.host: 0.0.0.0
+http.port: 9200
+discovery.type: single-node
+action.auto_create_index: false
+plugins.security.disabled: true
+indices.query.bool.max_clause_count: 32768" | sudo tee /etc/opensearch/opensearch.yml
+
+# Configure the kernel parameters at runtime
+# See https://opensearch.org/docs/latest/install-and-configure/install-opensearch/index/#important-settings
+sudo sysctl -w vm.max_map_count=262144
+echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.conf
+
+# enable and start opensearch service
+sudo systemctl daemon-reload
+sudo systemctl enable opensearch.service
+sudo systemctl start opensearch.service
+
+# =============================================================================
+# MongoDB
+
+# for reference, via https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+# Install MongoDB 6
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update
+sudo apt install -y mongodb-org mongodb-mongosh
+
+# Enable MongoDB service
+sudo systemctl daemon-reload
+sudo systemctl enable mongod.service
+sudo systemctl start mongod.service
+
+# =============================================================================
+# Graylog
+wget https://packages.graylog2.org/repo/packages/graylog-5.2-repository_latest.deb
+sudo dpkg -i graylog-5.2-repository_latest.deb
+sudo apt update && sudo apt install -y graylog-enterprise
+
+sudo cp /etc/graylog/server/server.conf server.conf.bak
+tmppw=$(echo "admin" | tr -d '\n' | sha256sum | cut -d" " -f1)
+sudo sed -i "s/root_password_sha2 =.*/root_password_sha2 = $tmppw/g" /etc/graylog/server/server.conf
+
+tmppw=$(openssl rand -hex 32)
+sudo sed -i "s/password_secret =.*/password_secret = $tmppw/g" /etc/graylog/server/server.conf
+tmppw=abc
+
+sudo sed -i 's/#http_bind_address = 127.0.0.1.*/http_bind_address = 0.0.0.0:9000/g' /etc/graylog/server/server.conf
+
+tmpip=127.0.0.1
+sudo sed -i "s/#elasticsearch_hosts = .*/elasticsearch_hosts = http\:\/\/$tmpip\:9200/g" /etc/graylog/server/server.conf
+
+sudo systemctl daemon-reload
+sudo systemctl enable graylog-server
+sudo systemctl start graylog-server
