ebcc7a4aed4c49ddf1426343078be89f80a8a10a
Drew Miranda <drew.miranda@graylog.com>
2024-06-24 14:16:54 -0400

    add setting proper vm.max_map_count and enabling service

--- opensearch/install-opensearch-deb.sh
+++ opensearch/install-opensearch-deb.sh
@@ -27,6 +27,15 @@ echo "deb [signed-by=/usr/share/keyrings/opensearch-keyring] https://artifacts.o
 sudo apt-get update
 sudo OPENSEARCH_INITIAL_ADMIN_PASSWORD=$(tr -dc A-Z-a-z-0-9_@#%^-_=+ < /dev/urandom  | head -c${1:-32}) apt-get -y install opensearch
 
+# Configure the kernel parameters at runtime
+# See https://opensearch.org/docs/latest/install-and-configure/install-opensearch/index/#important-settings
+sudo sysctl -w vm.max_map_count=262144
+echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.conf
+
+# enable and start opensearch service
+sudo systemctl daemon-reload
+sudo systemctl enable opensearch.service
+
 echo "Install Completed"
 echo "NOTE: You may need to start/restart service opensearch:"
 echo "systemctl restart opensearch"
