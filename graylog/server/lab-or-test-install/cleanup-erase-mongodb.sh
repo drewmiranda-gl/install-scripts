fd09ca1d6aa6ab7167a379ff1e83d66a73905971
Drew Miranda <drew.miranda@graylog.com>
2024-07-23 14:52:26 -0400

    add for cleanup

---
+++ graylog/server/lab-or-test-install/cleanup-erase-mongodb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+root_check() {
+    get_curusr=$(whoami)
+    if [ $get_curusr == "root" ]
+    then
+        ok="okhere"
+    else
+        echo "ERROR! Please run as root."
+        echo "Try: 'sudo su' to elevate and then run install script again."
+	echo "OR sudo bash $0"
+        exit 1
+    fi
+}
+root_check
+
+printf "${RED}WARNING! This will ERASE the entire 'graylog' colleciton in MongoDB${NC}\n"
+echo "This cannot be undone!"
+read -p "Confirmation 1 of 2: Press [Enter] key to confirm..."
+read -p "Confirmation 2 of 2: Press [Enter] key to confirm..."
+
+mongosh graylog --eval 'db.dropDatabase()'
