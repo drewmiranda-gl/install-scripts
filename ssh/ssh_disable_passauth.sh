91e65e2ab99104c9cd7303b237e24b4f399b1b89
Drew Miranda <drew.miranda@graylog.com>
2024-08-11 16:35:41 -0400

    add

---
+++ ssh/ssh_disable_passauth.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+root_check() {
+    get_curusr=$(whoami)
+    if [ $get_curusr == "root" ]
+    then
+        ok="okhere"
+    else
+        echo "ERROR! Please run as root."
+        echo "Try: 'sudo su' to elevate and then run install script again."
+	echo "OR sudo bash $0"
+        exit 1
+    fi
+}
+root_check
+
+sed -i "s/^#\?PasswordAuthentication [[:alnum:]]\+/PasswordAuthentication no/g" /etc/ssh/sshd_config
