92898051b67af7ec3433750cab0876bccc69abc1
Drew Miranda <drew.miranda@graylog.com>
2025-06-04 16:42:31 -0400

    update to latest and add repo link

--- graylog/sidecar/windows/install-graylog-sidecar.cmd
+++ graylog/sidecar/windows/install-graylog-sidecar.cmd
@@ -5,7 +5,9 @@ del graylog_sidecar_installer.exe
 
 @REM Download
 @REM NOTE: this appears to be very slow (vs downloading in a web browser or wget/curl), unsure why. Using this method since we can't gaurentee curl/wget are available.
-powershell -Command "& {Invoke-WebRequest -URI https://github.com/Graylog2/collector-sidecar/releases/download/1.5.0/graylog_sidecar_installer_1.5.0-1.exe -OutFile graylog_sidecar_installer.exe}"
+@REM Find the latest file via:
+@REM https://github.com/Graylog2/collector-sidecar/releases
+powershell -Command "& {Invoke-WebRequest -URI https://github.com/Graylog2/collector-sidecar/releases/download/1.5.1/graylog_sidecar_installer_1.5.1-1.exe -OutFile graylog_sidecar_installer.exe}"
 
 @REM Install
 "%~dp0graylog_sidecar_installer.exe" /S -SERVERURL=https://GRAYLOGSERVER.DOMAIN.COM/api -APITOKEN=YOURAPITOKEN
