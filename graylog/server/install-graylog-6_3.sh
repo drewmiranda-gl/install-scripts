a804b50433dbc7995cae3ae0df5785544ac80d19
Drew Miranda <drew.miranda@graylog.com>
2025-07-10 10:04:59 -0400

    add 6.3

---
+++ graylog/server/install-graylog-6_3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+root_check() {
+    get_curusr=$(whoami)
+    if [ $get_curusr == "root" ]
+    then
+        ok="okhere"
+    else
+        echo "ERROR! Please run as root."
+        echo "Try: 'sudo su' to elevate and then run install script again."
+	echo "OR sudo bash $0"
+        exit 1
+    fi
+}
+root_check
+
+# =============================================================================
+# Graylog
+wget https://packages.graylog2.org/repo/packages/graylog-6.3-repository_latest.deb
+sudo dpkg -i graylog-6.3-repository_latest.deb
+sudo apt update && sudo apt install -y graylog-enterprise
