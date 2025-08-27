45b45e326fd43b5fa63a3855797856c8d747d48b
Drew Miranda <drew.miranda@graylog.com>
2025-08-27 16:56:52 -0400

    fix mongo

--- mongodb/8.0/install-mongodb-ub22-jammy.sh
+++ mongodb/8.0/install-mongodb-ub22-jammy.sh
@@ -17,6 +17,35 @@ root_check
 # Prevent startup error: vm.max_map_count is too low
 sudo sysctl -w vm.max_map_count=262144
 echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.conf
+# mongo hates swapping
+sudo sysctl -w vm.swappiness=0
+echo 'vm.swappiness = 0' | sudo tee -a /etc/sysctl.conf
+
+# =============================================================================
+# Address the following startup warning
+# > We suggest setting the contents of sysfsFile to 0
+
+sudo mkdir -p "/etc/systemd/system/mongod.service.d/"
+echo "[Service]
+Environment=GLIBC_TUNABLES=glibc.pthread.rseq=0" | sudo tee /etc/systemd/system/mongod.service.d/override.conf
+
+# echo 0 | sudo tee /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none
+
+echo "[Unit]
+Description=Disable Transparent Hugepages (THP)
+DefaultDependencies=no
+After=sysinit.target local-fs.target
+Before=mongod.service
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c 'echo never | tee /sys/kernel/mm/transparent_hugepage/enabled > /dev/null && echo never | tee /sys/kernel/mm/transparent_hugepage/defrag && echo 0 | sudo tee /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none > /dev/null'
+[Install]
+WantedBy=basic.target" | sudo tee /etc/systemd/system/disable-transparent-huge-pages.service
+
+sudo systemctl daemon-reload
+sudo systemctl start disable-transparent-huge-pages
+sudo systemctl enable disable-transparent-huge-pages
+# =============================================================================
 
 # for reference, via https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
