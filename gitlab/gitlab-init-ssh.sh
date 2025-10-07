5f5fa40f04d1a6fac56f595df8accd0515e03f16
Drew Miranda <drew.miranda@graylog.com>
2025-10-07 15:32:26 -0400

    cleanup

--- gitlab/gitlab-init-ssh.sh
+++ gitlab/gitlab-init-ssh.sh
@@ -6,10 +6,6 @@ BLUE="\e[34m"
 YELLOW="\e[33m"
 ENDCOLOR="\e[0m"
 
-CURDIR=$(pwd)
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-
 SSH_KEY_FILE=~/.ssh/id_ed25519.pub
 
 if [ -f "${SSH_KEY_FILE}" ]; then
