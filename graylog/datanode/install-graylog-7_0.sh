01c394579eee39c243980e73b708c83ce15c79b3
Drew Miranda <drew.miranda@graylog.com>
2025-10-14 11:22:59 -0400

    a

---
+++ graylog/datanode/install-graylog-7_0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+root_check() {
+    get_curusr=$(whoami)
+    if [ $get_curusr == "root" ]
+    then
+        ok="okhere"
+    else
+        echo "ERROR! Please run as root."
+        echo "Try: 'sudo su' to elevate and then run install script again."
+	echo "OR sudo bash $0"
+        exit 1
+    fi
+}
+root_check
+
+# =============================================================================
+# Graylog
+wget https://packages.graylog2.org/repo/packages/graylog-7.0-repository_latest.deb
+sudo dpkg -i graylog-7.0-repository_latest.deb
+sudo apt update
+sudo apt install -y graylog-datanode
