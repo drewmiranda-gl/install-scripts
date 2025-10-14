2f9b19d4846d98663a74fe1a869857e343c16139
Drew Miranda <drew.miranda@graylog.com>
2025-10-14 11:41:11 -0400

    mongo should be installed prior to datanode

--- graylog/server/lab-or-test-install/install-graylog-and-datanode-7_0.sh
+++ graylog/server/lab-or-test-install/install-graylog-and-datanode-7_0.sh
@@ -15,6 +15,22 @@ root_check() {
 root_check
 
 # =============================================================================
+# MongoDB
+
+# for reference, via https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt update
+sudo apt install -y mongodb-org mongodb-mongosh
+
+# Enable MongoDB service
+sudo systemctl daemon-reload
+sudo systemctl enable mongod.service
+sudo systemctl start mongod.service
+
+# =============================================================================
 # DataNode
 
 # Configure the kernel parameters at runtime
@@ -37,23 +53,6 @@ tmppw=abc
 sudo systemctl daemon-reload
 sudo systemctl enable graylog-datanode
 sudo systemctl start graylog-datanode
-
-
-# =============================================================================
-# MongoDB
-
-# for reference, via https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt update
-sudo apt install -y mongodb-org mongodb-mongosh
-
-# Enable MongoDB service
-sudo systemctl daemon-reload
-sudo systemctl enable mongod.service
-sudo systemctl start mongod.service
 
 # =============================================================================
 # Graylog
