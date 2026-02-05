65016159db341418899d6bf807bcd2dd72c889b3
Drew Miranda <drew.miranda@graylog.com>
2026-02-05 09:45:29 -0500

    Remove since this is now included in the forwarder package

--- graylog/forwarder/install-graylog-fwd-ubuntu22.sh
+++ graylog/forwarder/install-graylog-fwd-ubuntu22.sh
@@ -24,9 +24,6 @@ echo "deb https://packages.graylog2.org/repo/debian/ forwarder-stable 6" |
     
 apt-get update && apt-get install -y graylog-forwarder
 
-sed -i '/^LimitNOFILE=64000.*/a AmbientCapabilities=CAP_NET_BIND_SERVICE' /usr/lib/systemd/system/graylog-forwarder.service && \
-    systemctl daemon-reload
-
 echo -ne "Enter Graylog forwarder_server_hostname: " && tmp=$(head -1 </dev/stdin) && \
     sed -i "s/^forwarder_server_hostname.*/forwarder_server_hostname = $tmp/g" /etc/graylog/forwarder/forwarder.conf
 
