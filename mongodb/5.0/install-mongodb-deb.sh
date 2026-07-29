f89144a317df26cabedaffa7e7cdb6ec23aaa5a0
Drew Miranda <drew.miranda@graylog.com>
2026-07-29 12:17:12 -0400

    fixes

--- mongodb/5.0/install-mongodb-deb.sh
+++ mongodb/5.0/install-mongodb-deb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# NOTE this is explicitly for Ubutnu 20 (Focal)
 
 root_check() {
     get_curusr=$(whoami)
@@ -22,6 +23,6 @@ echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.conf
 curl -fsSL https://pgp.mongodb.com/server-5.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-5.0.gpg \
    --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt update
 sudo apt install -y mongodb-org
